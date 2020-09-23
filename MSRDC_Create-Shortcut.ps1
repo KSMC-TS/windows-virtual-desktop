5ff01f87cf41a286e12abdf2a8d50823a485e5f2
Fred Gottman <phnxnmx@gmail.com>
2020-09-23 14:47:37 -0400

    desktop shortcut changes

--- MSRDC_Create-Shortcut.ps1
+++ MSRDC_Create-Shortcut.ps1
@@ -1,11 +1,12 @@
-param ([Parameter(Mandatory=$true)][string]$workspace)
+param ([Parameter(Mandatory=$true)][string]$workspace,$folder)
 $workspacename = "$workspace (RD)"
-$appdatapath = "$env:appdata\Microsoft\Windows\Start Menu\Programs\$workspacename"
+$appdatapath = "$env:appdata\Microsoft\Windows\Start Menu\Programs\$workspacename*"
+$userappdatapath = Get-ChildItem $appdatapath | Select-Object FullName
 $userdesktop = [Environment]::GetFolderPath("Desktop")
 $Shortcutfile = "$userdesktop\$workspacename.lnk"
 $appfile = "C:\Program Files\Remote Desktop\msrdcw.exe"
 $startup = "$env:appdata\Microsoft\Windows\Start Menu\Programs\Remote Desktop.lnk"
-
+[string]$userappdatapath = Get-ChildItem $appdatapath
 
 if (!(Test-Path $startup)) {
     $WScriptShell = New-Object -ComObject WScript.Shell
@@ -15,13 +16,31 @@ if (!(Test-Path $startup)) {
 }
 
 
-if (Test-Path $appdatapath) {
+if (Test-Path $userappdatapath) {
 
-    if (!(Test-Path $ShortcutFile)) {
-        $WScriptShell = New-Object -ComObject WScript.Shell
-        $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-        $Shortcut.TargetPath = $appdatapath
-        $Shortcut.Save()
+    if ($folder -eq $true) {
+        if (!(Test-Path $ShortcutFile)) {
+            $WScriptShell = New-Object -ComObject WScript.Shell
+            $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+            $Shortcut.TargetPath = $userappdatapath
+            $Shortcut.Save()
+        }
+    }
+    $appshortcuts = Get-ChildItem -Path $userappdatapath\*.lnk
+    $desktopshortcuts = Get-ChildItem -Path $userdesktop\*$workspace*.lnk -Exclude "$WorkspaceName.lnk"
+    foreach ($desktopshortcut in $desktopshortcuts) {
+        $filename = $desktopshortcut.PSChildName
+        $pathcheck = "$userappdatapath\$filename"
+        if (!(Test-Path $pathcheck)) { Remove-Item $desktopshortcut -Force }
+    }
+    foreach ($appshortcut in $appshortcuts) {
+        if(!(Test-Path $userdesktop\$appshortcut.Name)) {
+            Copy-Item $appshortcut $userdesktop -Force
+        } else {
+        #    Remove-Item $userdesktop\$appshortcut.Name -Force
+        #    Copy-Item $appshortcut $userdesktop -Force
+        }
+
     }
 
 } else {
